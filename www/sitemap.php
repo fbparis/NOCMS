0b41d846047e8d81b3ac9bcb1a4f5cbd7069875d
FB <fbparis@gmail.com>
2013-02-25 03:02:06 +0100

    Exclude symbolic links from sitemap

--- www/sitemap.php
+++ www/sitemap.php
@@ -16,12 +16,11 @@ while (@count($stack)) {
 	$d = dir(array_shift($stack));
 	$dirname = substr($d->path, strlen($base)) . '/';
 	while (false !== ($entry = $d->read())) {
-		if (($entry == '.') || ($entry == '..')) continue;
+		if (is_link($entry) || ($entry == '.') || ($entry == '..')) continue;
 		if (is_dir($d->path . '/' . $entry)) {
 			if (in_array($entry, $exclude_dir)) continue;
 			$stack[] = $d->path . '/' . $entry;	
-		} 
-		else if (preg_match('#^(.*)\.php$#s', $entry, $m)) {
+		} else if (preg_match('#^(.*)\.php$#s', $entry, $m)) {
 			if (in_array($m[1], $exclude_file)) continue;
 			printf("	<url><loc>http://%s%s</loc></url>\n", $hostname, $dirname . $m[1]);			
 		}
