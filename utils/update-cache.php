2c01177d8519119edb1d1e5559ebb59138c3db75
FB <fbparis@gmail.com>
2013-02-25 03:03:00 +0100

    Exclude symbolic links, remove old cache files

--- utils/update-cache.php
+++ utils/update-cache.php
@@ -11,6 +11,7 @@ $exclude_file = array();
 $exclude_dir = array();
 
 $base = dirname(__FILE__) . '/../www/.pages';
+$cache_mask = str_replace('%', '%%', dirname(__FILE__)) . '/../www/.cache%s%s.html';
 $stack = array($base);
 
 $_SERVER['REQUEST_METHOD'] = 'HEAD';
@@ -19,13 +20,13 @@ while (@count($stack)) {
 	$d = dir(array_shift($stack));
 	$dirname = substr($d->path, strlen($base)) . '/';
 	while (false !== ($entry = $d->read())) {
-		if (($entry == '.') || ($entry == '..')) continue;
+		if (is_link($entry) || ($entry == '.') || ($entry == '..')) continue;
 		if (is_dir($d->path . '/' . $entry)) {
 			if (in_array($entry, $exclude_dir)) continue;
 			$stack[] = $d->path . '/' . $entry;	
-		} 
-		else if (preg_match('#^(.*)\.php$#s', $entry, $m)) {
+		} else if (preg_match('#^(.*)\.php$#s', $entry, $m)) {
 			if (in_array($m[1], $exclude_file)) continue;
+			@unlink(sprintf($cache_mask, $dirname, $m[1]));
 			$_SERVER['REQUEST_URI'] = $dirname . $m[1];
 			if (class_exists('NOCMS')) {
 				NOCMS::template('default');
