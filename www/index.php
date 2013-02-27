8bd203e80555c8d172069e6ac3f38cb0029a4860
FB <fbparis@gmail.com>
2013-02-27 05:49:41 +0100

    Several updated
    - Added import method
    - Updated lastModified method
    - Updated write_cache method
    - Updated init method

--- www/index.php
+++ www/index.php
@@ -15,6 +15,7 @@ class NOCMS {
 	protected static $remoteHostname = null;
 	protected static $cachable = true;
 	protected static $html = '';
+	protected static $lambda = null;
 	
 	public static function uri() {
 		return self::$uri;
@@ -40,11 +41,25 @@ class NOCMS {
 	}
 	
 	public static function lastModified($t=null) {
-		if ($t === null) return self::$lastModified; else self::$lastModified = max($t, self::$lastModified);
+		if ((self::$lastModified !== null) && is_int($t)) self::$lastModified = max($t, self::$lastModified);
+		return self::$lastModified;
 	}
 	
 	public static function nocache() {
 		self::$cachable = false;
+	}
+	
+	public static function import() {
+		if (func_num_args() != 1) {
+			trigger_error('Invalid arguments for NOCMS::import() method');
+		} else if (is_readable(func_get_arg(0))) {
+			self::lastModified(filemtime(func_get_arg(0)));
+			extract($GLOBALS, EXTR_REFS);
+			include func_get_arg(0);
+			foreach(array_diff(array_keys(get_defined_vars()), array_keys($GLOBALS)) as self::$lambda) $GLOBALS[self::$lambda] =& ${self::$lambda}; 
+		} else {
+			trigger_error(sprintf('Include file not found: %s', func_get_arg(0)));
+		}
 	}
 	
 	public static function file_get_contents($f) {
@@ -73,12 +88,15 @@ class NOCMS {
 	protected static function write_cache() {
 		$filename = dirname(__FILE__) . sprintf(self::cache, self::$uri);
 		@mkdir(dirname($filename), 0777, true);
-		@file_put_contents($filename, self::$html);
-		@chmod($filename, 0666);
+		if ((self::$lastModified == null) || (@filemtime($filename) <= self::$lastModified)) {
+			@file_put_contents($filename, self::$html);
+			@chmod($filename, 0666);			
+		} 
 	}
 	
 	public static function init($cache=true) {
-		header('X-Powered-By: https://github.com/fbparis/NOCMS', true); 
+		header('X-Powered-By: https://github.com/fbparis/NOCMS', true);
+		self::$lastModified = filemtime(__FILE__);
 		self::$headers = array();
 		list(self::$uri, self::$args) = preg_split('/\?/s', $_SERVER['REQUEST_URI'], 2);
 		self::$uri = preg_replace('#/[/.~]+#s', '/', self::$uri);		
@@ -86,16 +104,15 @@ class NOCMS {
 		$page = dirname(__FILE__) . sprintf(self::pages, self::$uri);
 		if (is_readable($page)) {
 			ob_start();
-			include $page;
+			self::import($page);
 			self::$content = ob_get_clean();
 			if (is_readable(dirname(__FILE__) . sprintf(self::templates, self::$template))) {
 				ob_start();
-				include dirname(__FILE__) . sprintf(self::templates, self::$template);
-				foreach (self::$headers as $name=>$value) header("$name: $value");
+				self::import(dirname(__FILE__) . sprintf(self::templates, self::$template));
 				if ((self::$lastModified !== null) && ($_SERVER['REQUEST_METHOD'] != 'POST')) {
-					foreach (get_included_files() as $filename) self::lastModified(@filemtime($filename));
 					header('Last-Modified: ' . gmdate('D, d M Y H:i:s T', self::$lastModified), true);
 				}
+				foreach (self::$headers as $name=>$value) header("$name: $value");
 				self::$html = ob_get_clean();
 				if (self::$cachable) self::write_cache();
 			} else {
@@ -103,15 +120,15 @@ class NOCMS {
 				self::$html = 'Unknown template file';
 			}
 		} else {
-			header('X-Robots-Tag: noindex, follow, noarchive', true, 404);
 			if (is_readable(dirname(__FILE__) . sprintf(self::templates, '404'))) {
 				ob_start();
-				include sprintf(dirname(__FILE__) . self::templates, '404');
+				self::import(sprintf(dirname(__FILE__) . self::templates, '404'));
 				foreach (self::$headers as $name=>$value) header("$name: $value");
 				self::$html = ob_get_clean();
 			} else {
 				self::$html = 'Page not found';
 			}
+			header('X-Robots-Tag: noindex, follow, noarchive', true, 404);
 		}
 		flush();
 		if (in_array($_SERVER['REQUEST_METHOD'], array('GET','POST'))) echo self::$html;
